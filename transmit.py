3df03e7a4fdcadacc1c316a0abc3946999ef2cb7
s203932 <s203932@dtu.dk>
2024-04-10 11:04:11 +0200

    now with content

---
+++ transmit.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+import time
+import socket
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+
+# Write your program here.
+ev3.speaker.beep()
+
+# initilize motor
+feed = Motor(Port.C)
+
+feed.run(-40000)
+
+time.sleep(600)
+
+ev3.speaker.beep()
+
+
+# https://www.youtube.com/watch?v=8pMaR-WUc6U&t=605s&ab_channel=NeuralNine
