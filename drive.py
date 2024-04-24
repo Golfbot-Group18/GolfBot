8f13d0c71e64f75c1fa1a52dd1eb792b8387a1ae
s203932 <s203932@dtu.dk>
2024-04-24 11:54:23 +0200

    drive with added functions for movement

--- drive.py
+++ drive.py
@@ -13,19 +13,34 @@ import socket
 
 
 class Drive:
-    def singleMotor(ev3: EV3Brick, motor: Motor, run_time):
-        motor.run(40000)
+    def feedMotor(ev3: EV3Brick, motor: Motor, run_time, direction):
+        if(direction == "forward"):
+            motor.run(40000)
+        elif(direction == "backwards"):
+            motor.run(-40000)
+        else:
+            ev3.speaker.play_notes(['F4/4#','F4/4#','D4/4#','D4/4#'])
 
-        time.sleep(run_time*10)
 
-
+    def singleMotor(ev3: EV3Brick, motor: Motor, direction):
+        if(direction>0):
+            motor.run(400000)
+        else:
+            motor.run(-400000)
+        
+        
     def bothMotors(ev3: EV3Brick, motorRight:Motor, motorLeft:Motor, run_time):
-        motorLeft.run(4000000)
-        motorRight.run(400000)
+        motorLeft.run(-400)
+        motorRight.run(400)
 
         time.sleep(run_time)
 
-        motorLeft.hold()
-        motorRight.hold()
+        motorLeft.stop()
+        motorRight.stop()
+
+    def motorStop(ev3: EV3Brick, motor: Motor):
+        motor.stop()
+        
+
 
         
--- transmit.py
+++ transmit.py
@@ -11,33 +11,99 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 import time
 import socket
 
+# our own class for simple drive to motors
 import drive
 
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
 
+# setting up the class of functions that drives motors
 driveFunc = drive.Drive
 
-#listOfDevices = bluetooth
 
-
-
-# Create your objects here.
+# Creating the Ev3 brick
 ev3 = EV3Brick()
 
 
-# Write your program here.
+# Initialization beep
 ev3.speaker.beep()
+
+
+
 
 # initilize motor
 rightMotor = Motor(Port.B)
 leftMotor = Motor(Port.A)
 feed = Motor(Port.C)
 
+
+feed.run(40000)
+'''
+# setting up drivebase
+driveBase = DriveBase(leftMotor, rightMotor,75 ,185)
+driveBase.settings(30,30,30,30)
+#driveBase.stop
+
+
 #feed.run(40000)
 
 #time.sleep(600)
+
+
+# method to run single motor
+#driveFunc.singleMotor(ev3,leftMotor,10,1)
+#driveFunc.singleMotor(ev3,rightMotor,10,0)
+# method to run both motors
+#driveFunc.bothMotors(ev3, rightMotor, leftMotor, 10)
+
+
+
+#driveBase.straight(100)
+#time.sleep(10)
+#driveBase.turn(360)
+#time.sleep(10)
+#driveBase.straight(100)
+
+
+
+# next part is a simple test run where it will drive forward untill it detects
+# a ball with the color sensor, run the feed motor and pick up the ball
+
+color_Sensor = ColorSensor(Port.S2)
+
+driveBase.drive(-30,0)
+
+while(color_Sensor.reflection() == 0):
+    print(color_Sensor.reflection())
+#driveBase.stop()
+driveFunc.singleMotor(ev3,feed, 1)
+#driveBase.straight(-50)
+
+# second run/ ball
+#driveBase.drive(-30,0)
+
+#while(color_Sensor.reflection() == 0):
+#    print(color_Sensor.reflection())
+#driveBase.stop()
+#driveFunc.singleMotor(ev3,feed, 1)
+#driveBase.straight(-50)
+
+
+
+ev3.speaker.beep()
+
+
+
+
+
+
+
+
+'''
+
+
+
 '''
 color_Sensor = ColorSensor(Port.S2)
 
@@ -54,11 +120,3 @@ while(1):
 
 #while(1):
 #    print(ultra_Sensor.distance())
-
-
-#driveFunc.singleMotor(ev3,feed,4)
-driveFunc.bothMotors(ev3, rightMotor, leftMotor, 4)
-
-
-ev3.speaker.beep()
-
