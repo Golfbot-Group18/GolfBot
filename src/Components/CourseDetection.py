5c240e934560586088bbf6a7a85fe22c77cefd84
IntelliLakshmi <mettea652@gmail.com>
2024-04-26 15:05:44 +0200

    ny file, ny start

--- src/Components/CourseDetection.py
+++ src/Components/CourseDetection.py
@@ -2,51 +2,19 @@ import os
 import cv2
 import numpy as np
 
-# Get the current directory where your script is located
-script_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Provide the correct full path to the image file
-image_path = os.path.join(script_dir, '..', 'Data', 'Images', 'test1.jpg')
-# Load the image
+# Får den aktuelle mappe, hvor vores script ligger og den korrekte stig til billede filerne
+script_dir = os.path.dirname(__file__))
+image_path = os.path.join(script_dir, '..', 'data', 'images', 'Robot_green-jpg')
 img = cv2.imread(image_path)
 
+# Konverter fra BGR til HSV farverummet
+img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+def detect_red(img_hsv):
 
-# Function to detect red areas in the image
-def detect_red(image):
-    # Define the lower and upper bounds for the red color in HSV color space
-    lower_red = np.array([0, 100, 100])
-    upper_red = np.array([10, 255, 255])
+    # definer de lysere farveområder hsv
+    lower_red0 = np.array([0, 100, 100])
+    upper_red0 = np.array([5, 255, 255])
+    # definer de mørkere farveområder hsv
+    lower_red1 = np.array([0, 100, 100])
+    upper_red1 = np.array([5, 255, 255])
 
-    # Convert the image to HSV color space
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-    # Threshold the HSV image to get only red colors
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-
-   # Apply morphological operations to remove noise
-   # mask = cv2.erode(mask, None, iterations=2)
-   # mask = cv2.dilate(mask, None, iterations=2)
-
-    # Find contours in the mask
-    contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Draw contours on the original image
-    for contour in contours:
-        area = cv2.contourArea(contour)
-        if area > 1000:  # Adjust the area threshold as needed
-            cv2.drawContours(image, [contour], -1, (0, 0, 255), 2)
-
-    return image
-
-
-# Check if the image is loaded successfully
-if img is None:
-    print(f"Error: Unable to load the image from '{image_path}'.")
-else:
-    # Detect red areas in the image
-    course = detect_red(img)
-    cv2.namedWindow('Course Detected', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('Course Detected', 720, 1280)
-    cv2.imshow('Course Detected', course)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
