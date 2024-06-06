9ba4cca43cd3080c6034d8fcb79ebd1ffee458e8
Davidgtrivera <s215461@dtu.dk>
2024-06-06 15:36:07 +0200

    Print coordinates for egg

--- src/Components/TemporaryScripts/ImageAnalysisOnTestImage.py
+++ src/Components/TemporaryScripts/ImageAnalysisOnTestImage.py
@@ -26,7 +26,7 @@ else:
     # If balls are found, draw them on the image
     if balls is not None:
         balls = np.uint16(np.around(balls))
-
+        print("Ball Coordinates:")
         for i, circle in enumerate(balls[0, :]):
             # Draw the outer circle
             cv2.circle(frame, (circle[0], circle[1]), circle[2], (0, 255, 0), 2)
@@ -34,14 +34,14 @@ else:
             cv2.circle(frame, (circle[0], circle[1]), 2, (0, 0, 255), 3)
 
             label_position = (circle[0] - 10, circle[1] - 10)
-            cv2.putText(frame, "ball", label_position, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            cv2.putText(frame, "ball", label_position, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
             x, y, radius = circle
             print(f"Center coordinates: ({x}, {y}), Radius: {radius}")
 
     # If eggs are found, draw them on the image
     if egg is not None:
         egg = np.uint16(np.around(egg))
-
+        print("Egg Coordinates:")
         for i, circle in enumerate(egg[0, :]):
             # Draw the outer circle
             cv2.circle(frame, (circle[0], circle[1]), circle[2], (0, 255, 0), 2)
@@ -49,7 +49,9 @@ else:
             cv2.circle(frame, (circle[0], circle[1]), 2, (0, 0, 255), 3)
 
             label_position = (circle[0] - 10, circle[1] - 10)
-            cv2.putText(frame, "Egg", label_position, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            cv2.putText(frame, "Egg", label_position, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            x, y, radius = circle
+            print(f"Center coordinates: ({x}, {y}), Radius: {radius}")
 
 
 
