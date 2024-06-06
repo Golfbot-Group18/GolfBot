e8989e7e0babc4909dec949423ac448b3306290e
Davidgtrivera <s215461@dtu.dk>
2024-06-06 14:16:05 +0200

    Started on putting Egg and Ball detection together

--- src/Components/EggDetection.py
+++ src/Components/EggDetection.py
@@ -53,3 +53,25 @@ else:
     cv2.imshow('Eggs Detected',img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+def DetectEgg(frame):
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (9, 9), 2)
+
+    # Use Hough Circle Transform to detect circles. These values have been modified to fit ball detection FHD resolution
+    # Changing the visual size as well as resolution of picture of the balls will affect the circle detection
+    # Needs camera distance calibration to find the proper values
+    circles = cv2.HoughCircles(
+        blurred,
+        cv2.HOUGH_GRADIENT_ALT,
+        dp=1,
+        minDist=20,
+        param1=50,
+        param2=0.7,
+        minRadius=25,
+        maxRadius=30
+    )
+    if circles is not None:
+        return circles
