de01c0306b8421cdad2fd829ce5c6ccac7531cc7
s203932 <s203932@dtu.dk>
2024-06-21 10:07:48 +0200

    added the method for height displacement

--- src/Server/Utils/px_conversion.py
+++ src/Server/Utils/px_conversion.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 def calculate_scale_factor_from_ball(diameter_real_cm, ball_data):
     radii = ball_data[0, :, 2]
@@ -8,3 +9,42 @@ def calculate_scale_factor_from_ball(diameter_real_cm, ball_data):
     scale_factor = diameter_real_cm / diameter_pixel
     print(f"Scale factor: {scale_factor}")
     return scale_factor
+
+
+
+# To account for the height displacement of the robot, all in cm
+def realCoordinates (robotHeight: float, cameraHeight: float, robotCoordinates):
+    #I'm at the moment assuming that directly beneath the camera is the center of the 
+    # grid. This should always be the case if the camera is level. 
+    cameraCoordinates = (960,540)
+
+    x_diff = robotCoordinates[0]-cameraCoordinates[0]
+    #print(f'x_diff: {x_diff}')
+    y_diff = robotCoordinates[1]- cameraCoordinates[1]
+    #print(f'y_diff: {y_diff}')
+
+    r = math.sqrt (math.pow(x_diff,2)+math.pow(y_diff,2))
+    #print(f'r: {r}')
+
+    if r != 0: 
+
+
+        delta_f = cameraHeight-robotHeight
+
+        correction_factor = delta_f/cameraHeight
+        #print(f'correction_factor: {correction_factor}')
+
+        corrected_r = r*correction_factor
+        #print(f'corrected_r: {corrected_r}')
+
+        corrected_x = cameraCoordinates[0]+(x_diff/r)*correction_factor
+        corrected_y = cameraCoordinates[1]+(y_diff/r)*correction_factor
+
+        #print(f'correctionValue_x: {(x_diff/r)*correction_factor}')
+        #print(f'correctionValue_y: {(y_diff/r)*correction_factor}')
+
+        return corrected_x, corrected_y
+    
+    else: 
+        return robotCoordinates[0],robotCoordinates[1]
+
