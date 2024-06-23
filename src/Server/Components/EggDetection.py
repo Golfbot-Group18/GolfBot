3d894f356556793ff4acecfba89a3fb5d61ef033
Davidgtrivera <s215461@dtu.dk>
2024-06-23 21:29:11 +0200

    Update EggDetection.py

--- src/Server/Components/EggDetection.py
+++ src/Server/Components/EggDetection.py
@@ -1,5 +1,5 @@
 import cv2
-from Components.DetectionMethods import *
+from src.Server.Components.DetectionMethods import *
 
 
 def DetectEgg(frame):
@@ -7,8 +7,6 @@ def DetectEgg(frame):
     max_canny_threshold = 200
     min_ellipse_size = (30, 30)  # Minimum width and height of the ellipse
     max_ellipse_size = (100, 100)  # Maximum width and height of the ellipse
-    _, thresh = cv2.threshold(frame, 250, 255, cv2.THRESH_BINARY)
-    eggs, _ = detectionMethods.DetectEllipse(thresh, min_ellipse_size, max_ellipse_size, min_canny_threshold, max_canny_threshold)
     _, thresh = cv2.threshold(frame, 250, 255, cv2.THRESH_BINARY)
     eggs, _ = DetectEllipse(thresh, min_ellipse_size, max_ellipse_size, min_canny_threshold, max_canny_threshold)
     return eggs
